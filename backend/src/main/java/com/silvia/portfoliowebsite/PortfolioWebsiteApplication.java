b98adecf416e4eb46ece2e692816b3e042948521
Silvia <silvia940327@gmail.com>
2025-11-17 10:26:28 -0500

    adding initial backend spring files

---
+++ backend/src/main/java/com/silvia/portfoliowebsite/PortfolioWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.silvia.portfoliowebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioWebsiteApplication.class, args);
+	}
+
+}
