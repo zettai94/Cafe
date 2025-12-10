f4df07fd2cccd9e4ee9c3aaf398674d51b342b45
Silvia <silvia940327@gmail.com>
2025-12-10 10:55:04 -0500

    add bootstrap

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Portfolio from './Portfolio';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>
+);
+
+
